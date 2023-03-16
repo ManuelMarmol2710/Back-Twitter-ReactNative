41e383d681b80a5b1c3faf3019cc5ba11be99e6b
ManuelMarmol2710 <92897105+ManuelMarmol2710@users.noreply.github.com>
2023-03-16 17:23:11 -0400

    Add files via upload

--- controllers/follow.controller.ts
+++ controllers/follow.controller.ts
@@ -35,7 +35,6 @@ export const getFollows =async (req:Request,res:Response) => {
 
 export const getFollowersAndTweets = async (req: Request, res: Response)  =>  {
   const owner = await follow.find({owner: req.params.owner}) 
-
   let tweetsFollowing=[]
   for(var i of owner){
 const seguidores = i.following
